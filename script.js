567b9d8cda70da6faa0814e11606788f0c334efe
okanrav <bilirimrav@gmail.com>
2026-07-21 12:11:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,22 @@ const btn = document.getElementById("btn");
 const sonuc = document.getElementById("sonuc");
 
 btn.addEventListener("click", () => {
-    sonuc.textContent = "🤖 OKAN AI ODDS analizi yakında hazır olacak!";
+    const mac = document.getElementById("match").value.trim();
+
+    if (mac === "") {
+        sonuc.innerHTML = "⚠️ Lütfen bir maç girin.";
+        return;
+    }
+
+    sonuc.innerHTML = `
+    <h3>${mac}</h3>
+
+    ✅ Yapay Zeka Güven Puanı: <b>%82</b><br><br>
+
+    🎯 Maç Sonucu: <b>1</b><br>
+    ⚽ 2.5 Üst: <b>Evet</b><br>
+    🤝 KG Var: <b>Evet</b><br>
+    🟨 İlk Yarı 0.5 Üst: <b>Evet</b><br>
+    📈 Tahmini Skor: <b>2-1</b>
+    `;
 });
