6c9b88e49693a7f5b02cac00c71e261dfdb77904
okanrav <bilirimrav@gmail.com>
2026-07-21 12:06:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const sonuc = document.getElementById("sonuc");
+
+btn.addEventListener("click", () => {
+    sonuc.textContent = "🤖 OKAN AI ODDS analizi yakında hazır olacak!";
+});
