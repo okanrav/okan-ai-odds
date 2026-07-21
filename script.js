3f399946b3f60274293ff10b6f0e53003cc37621
okanrav <bilirimrav@gmail.com>
2026-07-21 12:15:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ btn.addEventListener("click", () => {
     const mac = document.getElementById("match").value.trim();
 
     if (mac === "") {
-        sonuc.innerHTML = "⚠️ Lütfen bir maç girin.";
+        sonuc.innerHTML = "<h3>⚠️ Lütfen bir maç giriniz.</h3>";
         return;
     }
 
     sonuc.innerHTML = `
-    <h3>${mac}</h3>
+    <div class="card">
+        <h2>⚽ ${mac}</h2>
 
-    ✅ Yapay Zeka Güven Puanı: <b>%82</b><br><br>
+        <p>🤖 Yapay Zeka Güven Puanı: <b>%82</b></p>
 
-    🎯 Maç Sonucu: <b>1</b><br>
-    ⚽ 2.5 Üst: <b>Evet</b><br>
-    🤝 KG Var: <b>Evet</b><br>
-    🟨 İlk Yarı 0.5 Üst: <b>Evet</b><br>
-    📈 Tahmini Skor: <b>2-1</b>
+        <p>🎯 Maç Sonucu: <b>1</b></p>
+
+        <p>⚽ 2.5 Üst: <b>Evet</b></p>
+
+        <p>🤝 Karşılıklı Gol: <b>Evet</b></p>
+
+        <p>🟨 İlk Yarı 0.5 Üst: <b>Evet</b></p>
+
+        <p>📈 Tahmini Skor: <b>2-1</b></p>
+    </div>
     `;
 });
