3812a3fe359ef2e0b1352aaf494b587b5024f459
okanrav <bilirimrav@gmail.com>
2026-07-21 14:17:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,96 @@ function filterMatches(){
     drawMatches(filtered);
 
 }
+
+function drawMatches(matches) {
+
+    if(matches.length===0){
+
+        sonuc.innerHTML="<div class='loading'>Maç bulunamadı.</div>";
+        return;
+
+    }
+
+    let html="";
+
+    matches.forEach(match=>{
+
+        let durum="🕒 Başlamadı";
+
+        if(match.fixture.status.short==="LIVE")
+            durum="🟢 Canlı";
+
+        if(match.fixture.status.short==="FT")
+            durum="✅ Bitti";
+
+        html+=`
+
+        <div class="card">
+
+            <div class="league">
+                🏆 ${match.league.name}
+            </div>
+
+            <div class="time">
+
+                ${durum}
+
+                <br><br>
+
+                ${match.fixture.date.substring(11,16)}
+
+            </div>
+
+            <div class="teams">
+
+                <div>
+
+                    <img src="${match.teams.home.logo}" width="45">
+
+                    <br>
+
+                    ${match.teams.home.name}
+
+                </div>
+
+                <strong>VS</strong>
+
+                <div>
+
+                    <img src="${match.teams.away.logo}" width="45">
+
+                    <br>
+
+                    ${match.teams.away.name}
+
+                </div>
+
+            </div>
+
+            <div class="ai-box">
+
+                <div class="ai-title">
+
+                    🤖 AI Analizi
+
+                </div>
+
+                <button class="ai-btn">
+
+                    Analizi Gör
+
+                </button>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+    sonuc.innerHTML=html;
+
+}
+
+loadMatches();
